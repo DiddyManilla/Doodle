169301459891a03b393cb32d178418f145904808
DiddyManilla <diddymanilla@Gmail.com>
2016-07-28 18:33:36 -0600

    Allow user to draw in black in one size on the canvas

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+var mouseButtonDown = false;
+function getMousePos(event) {
+	var rect = $("#canvas")[0].getBoundingClientRect();
+	return {
+		x: event.pageX - rect.left,
+		y: event.pageY - rect.top
+	}
+}
 $(document).ready(function() {
-	let canvas = document.getElementById("doodle-box");
-	let context = canvas.getContext('2d');
+
+	var canvas = document.getElementById("canvas");
+	var context = canvas.getContext('2d');
+	context.fillStyle = "#ffffff";
+	context.fillRect(0, 0, canvas.width * 2, canvas.height);
+	
+	$("#canvas").on('mousedown', () => mouseButtonDown = true);
+	$("#canvas").on('mouseup', () => mouseButtonDown = false);
+	
+	$("#canvas").on('mousemove', function(event) {
+		if (mouseButtonDown) {
+			context.fillStyle = "#000000";
+			var mousePos = getMousePos(event);
+			context.fillRect(mousePos.x, mousePos.y, 10, 10);
+		}
+	});
 });
